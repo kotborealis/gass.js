1ad913b52b7c867413dd495e1612e181bcd9d44b
kotborealis <kotborealis@gmail.com>
2020-04-21 12:33:31 +0300

    updated

---
+++ src/World.js
@@ -0,0 +1,78 @@
+
+
+class World {
+    /** @type {Atom[]} **/
+    atoms;
+
+    /** @type {Wall[]} **/
+    walls;
+
+    /**
+     *
+     * @param {Atom[]} atoms
+     * @param {Wall[]} walls
+     */
+    constructor(atoms, walls) {
+        this.atoms = atoms;
+        this.walls = walls;
+    }
+}
+
+
+
+
+
+
+
+/**
+ *
+ * @param {Number} delta
+ * @param {World} world
+ * @returns {World}
+ */
+const runPhysics = (delta, world) => {
+    if(delta === 0) {
+        return world
+    }
+
+    const collisions_ = [];
+
+    for(let i = 0; i < world.atoms.length; i++) {
+        for(let j = i + 1; j < world.atoms.length; j++)
+            collisions_.push(collideAtomAtom(delta, world.atoms[i], world.atoms[j]));
+
+        for(let j = 0; j < world.walls.length; j++)
+            collisions_.push(collideAtomWall(delta, world.atoms[i], world.walls[j]));
+    }
+
+    const collisions = collisions_.filter(id => id);
+
+    if(!collisions.length) {
+        return new World(
+            integrateAtoms(delta, world.atoms),
+            world.walls
+        );
+    }
+
+    const firstCollision = minCollision(collisions);
+    const tFirst = firstCollision.time;
+
+    if(tFirst > delta) {
+        return new World(
+            integrateAtoms(delta, world.atoms),
+            world.walls
+        );
+    }
+
+    const delta_ = delta - tFirst - 0.001;
+    const collidingAtoms = collisionAtoms(firstCollision);
+    const nonCollidingAtoms = world.atoms.filter(atom => !collidingAtoms.includes(atom));
+
+    return new World(
+        [
+            ...integrateAtoms(tFirst, nonCollidingAtoms),
+            ...resolveCollision(firstCollision)
+        ],
+        world.walls
+    );
+};
--- src/index.js
+++ src/index.js
@@ -1,101 +1,17 @@
-
-
-class World {
-    /** @type {Atom[]} **/
-    atoms;
-
-    /** @type {Wall[]} **/
-    walls;
-
-    /**
-     *
-     * @param {Atom[]} atoms
-     * @param {Wall[]} walls
-     */
-    constructor(atoms, walls) {
-        this.atoms = atoms;
-        this.walls = walls;
-    }
-}
-
-
-
-
-
-
-
-/**
- *
- * @param {Number} delta
- * @param {World} world
- * @returns {World}
- */
-const runPhysics = (delta, world) => {
-    if(delta === 0) {
-        return world
-    }
-
-    const collisions_ = [];
-
-    for(let i = 0; i < world.atoms.length; i++) {
-        for(let j = i + 1; j < world.atoms.length; j++)
-            collisions_.push(collideAtomAtom(delta, world.atoms[i], world.atoms[j]));
-
-        for(let j = 0; j < world.walls.length; j++)
-            collisions_.push(collideAtomWall(delta, world.atoms[i], world.walls[j]));
-    }
-
-    const collisions = collisions_.filter(id => id);
-
-    if(!collisions.length) {
-        return new World(
-            integrateAtoms(delta, world.atoms),
-            world.walls
-        );
-    }
-
-    const firstCollision = minCollision(collisions);
-    const tFirst = firstCollision.time;
-
-    if(tFirst > delta) {
-        return new World(
-            integrateAtoms(delta, world.atoms),
-            world.walls
-        );
-    }
-
-    const delta_ = delta - tFirst - 0.001;
-    const collidingAtoms = collisionAtoms(firstCollision);
-    const nonCollidingAtoms = world.atoms.filter(atom => !collidingAtoms.includes(atom));
-
-    return new World(
-        [
-            ...integrateAtoms(tFirst, nonCollidingAtoms),
-            ...resolveCollision(firstCollision)
-        ],
-        world.walls
-    );
-};
-
-
-
 const canvas = document.querySelector('canvas');
-const onResize = () => {
-    canvas.width  = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-onResize();
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
+canvas.width = width;
+canvas.height = height;
+
 const initialBounds = [
-    new Vector(0, 0),
-    new Vector(width, height)
+    new Vector(100, 100),
+    new Vector(width - 100, height - 100)
 ];
 
 const atomRadius = 10;
-
 const atomsCountX = 10;
 const atomsCountY = 10;
 
@@ -111,28 +27,26 @@ for(let i = 0; i < atomsCountX; i++)
             Vector.random(-10, 10, 0, 100),
         ));
 
-const walls = [
+const walls = (bounds = initialBounds) => [
     new Wall(
-        new Vector(initialBounds[0].x, initialBounds[0].y),
-        new Vector(initialBounds[1].x, initialBounds[0].y),
+        new Vector(bounds[0].x, bounds[0].y),
+        new Vector(bounds[1].x, bounds[0].y),
     ),
     new Wall(
-        new Vector(initialBounds[1].x, initialBounds[0].y),
-        new Vector(initialBounds[1].x, initialBounds[1].y),
+        new Vector(bounds[1].x, bounds[0].y),
+        new Vector(bounds[1].x, bounds[1].y),
     ),
     new Wall(
-        new Vector(initialBounds[1].x, initialBounds[1].y),
-        new Vector(initialBounds[0].x, initialBounds[1].y),
+        new Vector(bounds[1].x, bounds[1].y),
+        new Vector(bounds[0].x, bounds[1].y),
     ),
     new Wall(
-        new Vector(initialBounds[0].x, initialBounds[1].y),
-        new Vector(initialBounds[0].x, initialBounds[0].y),
+        new Vector(bounds[0].x, bounds[1].y),
+        new Vector(bounds[0].x, bounds[0].y),
     ),
 ];
 
-let world = new World(atoms, walls);
-
-document.addEventListener('resize', onResize);
+let world = new World(atoms, walls());
 
 const render = (canvas, world, delta) => {
     const ctx = canvas.getContext('2d');
@@ -161,19 +75,15 @@ const render = (canvas, world, delta) => {
 let lastTime = Date.now();
 let time = Date.now();
 
-let collisionNormals = [];
 
 const update = () => {
     time = Date.now();
     const delta = (time - lastTime)/1000;
-    //const delta = 1/60;
     lastTime = time;
+
     render(canvas, world, delta);
-    collisionNormals = [];
     world = runPhysics(delta, world);
     requestAnimationFrame(update);
 };
 
 update();
-
-window.canvas = canvas;
