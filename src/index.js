477ecd5910770396c4161eb7163e3e088e23ffe7
kotborealis <kotborealis@gmail.com>
2020-05-03 13:46:42 +0300

    integral params

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {generateWorld, runPhysics, World} from './entities/World';
+import {generateWorld, runPhysics} from './entities/World';
 import {renderWorld} from './render/renderWorld';
 import {Wall} from './entities/Wall';
 import {Vector} from './vector/Vector';
@@ -48,6 +48,26 @@ const render = (canvas, world) => {
 };
 
 render(canvas, world);
+
+const updateIntegralParams = (container, world) => {
+    const {walls, atoms} = world.current;
+
+    const volume = (walls[1].a.x - walls[0].a.x) * (walls[1].b.y - walls[1].a.y);
+    container.querySelector('.volume > .value').innerHTML = volume.toString();
+
+    const Ek_m = atoms
+        .map(({velocity}) => velocity.lengthSquared())
+        .reduce((a, b) => a + b)/atoms.length/2;
+    const pressure = 2/3 * (atoms.length / volume) * Ek_m;
+    container.querySelector('.pressure > .value').innerHTML = pressure.toString();
+
+    const temperature = Ek_m / (3/2) / (1.38 * 10e-23);
+    container.querySelector('.temperature > .value').innerHTML = temperature.toString();
+
+    setTimeout(updateIntegralParams.bind(this, container, world), 500);
+}
+
+updateIntegralParams(document.querySelector('.integral-params'), world);
 
 const update = (delta = 1/60) => {
     const timeStart = Date.now();
